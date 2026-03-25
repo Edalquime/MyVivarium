24139d0d4c3067cab57ab4fd3982448df079d944
Edalquime <eduardoquinonesmedina@gmail.com>
2026-03-25 14:56:48 -0300

    Update bc_prnt_crd.php
    Signed-off-by: Edalquime <eduardoquinonesmedina@gmail.com>

--- bc_prnt_crd.php
+++ bc_prnt_crd.php
@@ -1,5 +1,8 @@
 <?php
-
+// 🔥 ESTO MOSTRARÁ EL ERROR REAL EN PANTALLA
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 /**
  * Breeding Cage Printable Card Script
  *
